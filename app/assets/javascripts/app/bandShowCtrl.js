d425d5c5a491e93859515f3d1cc27f195c4a2e79
juanderuiz <juanderuiz@gmail.com>
2015-05-20 19:11:04 +0200

    Add a nf-if message to notify that the user can't delete a band

--- app/assets/javascripts/app/bandShowCtrl.js
+++ app/assets/javascripts/app/bandShowCtrl.js
@@ -3,6 +3,7 @@ angular
   .controller('bandShowCtrl', function($scope, Band, $routeParams, $location, session) {
     $scope.band = Band.get({id: $routeParams.id});
     $scope.albums = null;
+    $scope.caution = null; //To show the proper message in the template
 
     //To remove a band
     $scope.user = session.user;
@@ -12,9 +13,11 @@ angular
   	  if ($scope.user.id == $scope.band.user_id){ //The current user is the owner
         Band.delete({id: id});
         console.log("Borrando " + $scope.band.name);
+        $scope.caution = false;
         $location.path('/bands');
       } else {// The current user is NOT the owner, can't delete it
-      	console.log("Not your band!!!")
+      	console.log("Not your band!!!");
+        $scope.caution = true;
       }
     }
 });
