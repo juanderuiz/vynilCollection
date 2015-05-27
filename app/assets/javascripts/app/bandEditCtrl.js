f01e76dde55275bd53b29b4433ee4425bdfb1b78
juanderuiz <juanderuiz@gmail.com>
2015-05-27 14:14:36 +0200

    Can edit if you're not the owner

--- app/assets/javascripts/app/bandEditCtrl.js
+++ app/assets/javascripts/app/bandEditCtrl.js
@@ -1,9 +1,16 @@
 angular
   .module('myApp.bandEditCtrl',[])
-  .controller('bandEditCtrl', function($scope, Band, $routeParams, $location) {
-    $scope.band = Band.get({id: $routeParams.id});
-    
-    //What if I put the link in the navigator and the user is not the owner?
+  .controller('bandEditCtrl', function($scope, Band, $routeParams, $location, session) {
+    //$scope.band = Band.get({id: $routeParams.id});
+    Band.get({id: $routeParams.id}).$promise.then(function(band) {
+      $scope.band = band;
+      //If the current user is the owner, we will show buttons in the template
+      $scope.owner = ($scope.user.id == $scope.band.user_id);
+      console.log("Owner editing? " + $scope.owner);
+    });
+
+    //We need the user to get his ID
+    $scope.user = session.user;
 
   	$scope.saveBand = function(band){
       $scope.isSubmitting = true;
